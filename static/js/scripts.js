10378d19b2001d8f81108eb30e8ae71108481d58
zpneal <zpneal@msu.edu>
2026-01-05 14:20:36 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['section1', 'section2', 'section3']
+const section_names = ['section1', 'section2', 'section3', 'section4']
 
 
 window.addEventListener('DOMContentLoaded', event => {
